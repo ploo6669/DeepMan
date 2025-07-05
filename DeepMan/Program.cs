ea9fba0f71d0f248a79615fb3d6066546c7f47b2
ploo6669 <ploo6669@outlook.com>
2025-07-05 04:18:11 +0300

    Update Program.cs
    added input - output controller

--- DeepMan/Program.cs
+++ DeepMan/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// See https://aka.ms/new-console-template for more information
+
+while (true)
+{
+    var input = Console.ReadLine();
+
+    Console.WriteLine(Answer(input));
+}
+string Answer(string question)
+{
+    return "the answer is "+question;
+}
