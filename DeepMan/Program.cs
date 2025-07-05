c2d0b4b7c9242a3f78e6730f68d3f519152571f2
ploo6669 <ploo6669@outlook.com>
2025-07-05 10:54:31 +0300

    fixed docker

--- DeepMan/Program.cs
+++ DeepMan/Program.cs
@@ -10,3 +10,22 @@ string Answer(string question)
 {
     return "the answer is "+question;
 }
+public class LM
+{
+    public char A;
+    public char B;
+    public double C;
+}
+public class LMB
+{
+    public List<LM> data = new List<LM>();
+    public LMB(string question)
+    {
+        var r = new Random();
+        var ca = question.ToCharArray();
+        for (int i = 0; i < ca.Length-1; i++)
+        {
+            data.Add(new LM { A = ca[i], B = ca[i + 1], C = r.NextDouble() });
+        }
+    }
+}
