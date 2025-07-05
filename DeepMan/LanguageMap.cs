0a745dc03b6f15eaa63a10efee1878fbe352e6cd
ploo6669 <ploo6669@outlook.com>
2025-07-05 05:42:51 +0300

    added headers to the app

---
+++ DeepMan/LanguageMap.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeepMan
+{
+    public abstract class LanguageMap: BrainMap
+    {
+        public LanguageMap defaultPoint;
+        public LanguageMap endPoint;
+    }
+}
---
+++ DeepMan/LanguageModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeepMan
+{
+    public class LanguageModel
+    {
+        public Dictionary<CharacterModel, double> CharacterMap;
+    }
+}
